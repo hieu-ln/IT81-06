47de4b5c853596c6e3efad27d8ebeb2834907996
phuoc1310 <1851050120phuoc@ou.edu.vn>
2019-08-10 22:11:27 +0700

    Commit

--- CODEC5/HuuPhuoc_C5_bai5.cpp
+++ CODEC5/HuuPhuoc_C5_bai5.cpp
@@ -1,176 +1,321 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
+#include <sstream>
 using namespace std;
-#define MAX 20
-int a[MAX][MAX];
-int n;
-char vertex[MAX];
 
-void initGraph()
+#define MAX 20
+struct Node 
 {
+	int info;
+	Node *link;
+};
+
+// DSLK su dung cho stack, queue va danh sach ke
+Node *First[MAX]; // DS cac DSK
+int n;// so dinh do thi
+char vertex[MAX]; // ten dinh
+
+Node *sp;
+Node *front, *rear;
+
+// stack
+void initStack()
+{
+	sp = NULL;
+}
+
+int isEmptyS()
+{
+	if(sp == NULL)
+		return 1;
+	return 0;
+}
+
+void PushS(int x)
+{
+	Node *p = new Node;
+	p->info = x;
+	p->link = sp;
+	sp = p;
+}
+
+int PopS(int &x)
+{
+	if(sp != NULL) {
+		Node *p = sp;
+		x = p->info;
+		sp = p->link;
+		delete p;
+		return 1;
+	}
+	return 0;
+}
+// end Stack
+
+// Queue
+
+void initQueue()
+{
+	front = NULL;
+	rear = NULL; 
+}
+
+int isEmptyQ()
+{
+	if(front == NULL)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+void PushQ(int x)
+{
+	Node *p = new Node;
+	p->info = x;
+	p->link = NULL;
+	if(rear == NULL)
+		front = p;
+	else
+		rear->link = p;
+	rear = p;
+}
+
+int PopQ(int &x)
+{
+	if(front != NULL) {
+		Node *p = front;
+		front = p->link;
+		x = p->info;
+		if(front == NULL) {
+			rear = NULL;
+		}
+		delete p;
+		return 1;
+	}
+	return 0;
+}
+///end Queue
+
+void initGraph(int &n)
+{
+	for (int i = 0; i<n; i++)
+		First[i] = NULL;
 	n = 0;
+}
+
+void insertLast(Node *&first, Node *p)
+{
+	if(first == NULL)
+	{
+		first = p;
+	}
+	else
+	{
+		Node *q = first;
+		while(q->link != NULL)
+			q = q->link;
+		q->link = p;
+	}
 }
 
 void inputGraphFromText()
 {
 	string line;
-	ifstream myfile ("matrantrongso1.txt");
+	ifstream myfile ("danhsachke1.txt");
 	if(myfile.is_open())
 	{
 		myfile >> n;
 		for(int i = 0; i < n; i++)
 			myfile >> vertex[i];
-		for(int i = 0; i<n; i++)
+		string str;
+		int i = 0;
+		while(getline(myfile, str))
 		{
-			for (int j = 0; j < n; j++)
-				myfile >> a[i][j];
+			istringstream ss(str);
+			int u;
+			while (ss >> u)
+			{
+				// dinh u
+				Node *p = new Node;
+				p->info = u;
+				p->link=NULL;
+				insertLast(First[i-1], p);
+			}
+			i++;
 		}
 	}
 }
 
 void inputGraph()
 {
-	cout << "Nhap so dinh do thi n: ";
-	cin >>n;
+	cout << "Nhap so dinh do thi: ";
+	cin >> n;
 	cout << "Nhap ten dinh: ";
-	for(int i = 0; i< n; i++)
-		cin >> vertex[i];
 	for(int i = 0; i < n; i++)
+		cin >> vertex[i];
+	for(int i = 0; i < n + 1; i++)
 	{
-		cout << "Nhap vao dong thu " << i + 1 << ": ";
-		for(int j = 0; j< n; j++)
-			cin >> a[i][j];
+		if(i < 0)
+			cout << "Nhap DSK cua dinh thu " << i - 1<< " (" << vertex[i-1] << ") : ";
+		int u;
+		string str;
+		while(getline(cin, str))
+		{
+			istringstream ss(str);
+			while(ss >> u)
+			{
+				Node *p = new Node;
+				p->info = u;
+				p->link = NULL;
+				insertLast(First[i-1], p);
+			}
+		}
 	}
 }
 
 void outputGraph()
 {
-	cout << setw(6)<< left;
-	for(int i = 0; i < n; i++)
+	for (int i = 0; i<n; i++)
 	{
-		for(int j = 0; j < n; j++)
-			cout << a[i][j] << setw(6) << left;
+		cout << "Dinh " << i << " (" << vertex[i] << ") : ";
+		Node *p = First[i];
+		while(p != NULL)
+		{
+			cout << p->info << " ";
+			p = p->link;
+		}
 		cout << endl;
 	}
 }
 
-// khai bao
-
-int E1[MAX];
-int E2[MAX];
-int wE[MAX];
-int nE = 0;
-
-int T1[MAX];
-int T2[MAX];
-int wT[MAX];
-int nT = 0;
-int tonTai(int d, int D[], int nD)
+void output(int a[], int n)
 {
-	for(int i = 0; i < nD; i++)
-		if(D[i] == d)
-			return 1;
-	return 0;
+	for(int i = 0; i < n; i++)
+		cout << vertex[a[i]] << " ";
 }
 
-void xoaViTriE(int i)
+// BFS
+int C[100], bfs[100];
+int nbfs= 0;
+void initC()
 {
-	for (int j = i; j < nE; j++)
+	for(int i = 0; i<n; i++)
+		C[i] = 1;
+}
+
+void BFS(int v)
+{
+	int p;
+	Node *w;
+	PushQ(v);
+	C[v] = 0;
+	while(front!=NULL)
 	{
-		E1[j] = E1[j+1];
-		E2[j] = E2[j+1];
-		wE[j] = wE[j+1];
+		PopQ(p);
+		bfs[nbfs] = p;
+		w =First[p];
+		nbfs++;
+		while(w!=NULL)
+		{
+			if(C[w->info])
+			{
+				PushQ(w->info);
+				C[w->info]=0;
+			}
+			w=w->link;
+		}
 	}
 }
 
-void xoaCanhE(int u, int v)
+//DFS
+int dfs[100];
+int ndfs =0;
+void DFS(int s)
 {
-	for(int i = 0; i<nE;i++)
-		if(E1[i] == u&&E2[i]==v)
-		{
-			xoaViTriE(i);
-			break;
-		}
-}
-
-void prim(int s) // s la dinh bat dau
-{
-	int u =s, min, i, d1, d2;
-	while(nT<n-1)
+	PushS(s);
+	dfs[ndfs]=s;
+	ndfs++;
+	C[s] = 0;
+	int u=s;
+	Node *v = NULL;
+	while(isEmptyS() == 0)
 	{
-		for(int v = 0; v < n; v++)
-			if(a[u][v]!= 0)
-				if(tonTai(v, T2, nT) == 0)
-				{
-					E1[nE] = u;
-					E2[nE] = v;
-					wE[nE]=a[u][v];
-					nE++;
-				}
-		for(i = 0; i<nE;i++)
-			if(tonTai(E2[i], T2, nT) == 0)
+		if(v == NULL)
+			PopS(u);
+		v = First[u];
+		while(v!=NULL)
+		{
+			if(C[v->info]== 1)
 			{
-				min = wE[i];
-				d1=E1[i];
-				d2=E2[i];
+				PushS(u);
+				PushS(v->info);
+				dfs[ndfs]=v->info;
+				ndfs++;
+				C[v->info] =u;
+				u=v->info;
 				break;
 			}
-		for(;i<nE;i++)
-			if(tonTai(E2[i], T2, nT) == 0)
-				if(min > wE[i])
-				{
-					min = wE[i];
-					d1=E1[i];
-					d2=E2[i];
-				}
-		T1[nT] = d1;
-		T2[nT] = d2;
-		wT[nT] = a[d1][d2];
-		a[d1][d2] = 0;
-		a[d2][d1] = 0;
-		nT++;
-		xoaCanhE(d1,d2);
-		u = d2;
+			v=v->link;
+		}
 	}
 }
 
-void output(bool VertexName)
+void Search_by_BFS(int x, int v)
 {
-	int tong = 0;
-	for(int i = 0; i < nT; i++)
+	int p;
+	Node *w;
+	PushQ(v);
+	C[v]=0;
+	while(front !=NULL)
 	{
-		if(VertexName)
-			cout << endl << "("<< vertex[T1[i]] << "," << vertex[T2[i]]<<") = "<<wT[i];
+		PopQ(p);
+		if(x==p)
+		{
+			cout << "Tim thay x= " << x << endl;
+			return ;
+		}
 		else
-			cout << endl << "("<<T1[i] << "," <<T2[i]<<") = " << wT[i];
-		tong += wT[i];
+			cout << "Khong tim thay x" << endl;
+		w=First[p];
+		while(w!=NULL)
+		{
+			if(C[w->info])
+			{
+				PushQ(w->info);
+				C[w->info]=0;
+			}
+			w=w->link;
+		}
 	}
-	cout << "\nTong = " << tong;
 }
 
 void main()
-{
-	int choice, x, i;
-	cout << "============== BAI TAP 1, CHUONG 6, TIM CAY KHUNG TOI THIEU ================" << endl;
-	cout << "1. Khoi tao ma tran ke rong" << endl;
-	cout << "2. Nhap ma tran ke tu file text" << endl;
-	cout << "3. Nhap MA TRAN KE" << endl;
-	cout << "4. Xuat MA TRAN KE " << endl;
-	cout << "5. Tim CAY KHUNG TOI THIEU bang PRIM" << endl;
-	cout << "6. Thoat" << endl;
-	do {
-		cout << "Vui long chon so de thuc hien: ";
+{	
+	int choice, x;
+	system("cls");
+	cout << "------------- BAI TAP 4, CHUONG 5, DO THI -------------" << endl;
+	cout << "1. Khoi tao danh sach ke rong" << endl;
+	cout << "2. Nhap danh sach ke tu file text" << endl;
+	cout << "3. Nhap danh sach ke" << endl;
+	cout << "4. Xuat danh sach ke" << endl;
+	cout << "5. Duyet do thi theo chieu rong BFS-DSLK" << endl;
+	cout << "6. Duyet do thi theo chieu sau DFS-DSLK" << endl;
+	cout << "7. Tim dinh co gia tri x theo BFS" << endl;
+	cout << "8. Thoat" << endl;
+	do{
+		cout << "\nVui long chon so de thuc hien: ";
 		cin >> choice;
 		switch(choice)
 		{
 		case 1:
-			initGraph();
-			cout << "Ban vua khoi tao MA TRAN KE thanh cong!!" << endl;
+			n=10;
+			initGraph(n);
+			cout << "Ban vua khoi tao danh sach ke rong thanh cong" << endl;
 			break;
 		case 2:
 			inputGraphFromText();
-			cout << "Ban vua nhap MA TRAN KE tu FILE!!" << endl;
+			cout << "Ban vua nhap sanh sach ke tu file: \n";
 			outputGraph();
 			break;
 		case 3:
@@ -180,16 +325,38 @@ void main()
 			outputGraph();
 			break;
 		case 5:
+			initQueue();
+			initC();
 			cout << "Vui long nhap dinh xuat phat: ";
 			cin >> x;
-			prim(x);
-			cout << "Cay khung toi thieu PRIM: " << endl;
-			output(true);
+			ndfs=0;
+			BFS(x);
+			cout << "Thu tu dinh sau khi duyet BFS: ";
+			output(bfs, n);
 			break;
 		case 6:
-			cout << "------GOODBYE--------" << endl;
+			initStack();
+			initC();
+			cout << "Vui long nhap dinh xuat phat: ";
+			cin >> x;
+			ndfs=0;
+			DFS(x);
+			cout << "Thu tu dinh sau khi duyet DFS: ";
+			output(dfs, n);
+			break;
+		case 7:
+			initQueue();
+			initC();
+			ndfs=0;
+			cout << "Vui long nhap gia tri can tim: ";
+			cin >> x;
+			Search_by_BFS(x, 0);
+			break;
+		case 8:
+			cout << "Goobye!" << endl;
+			break;
 		default:
 			break;
 		}
-	} while(choice != 6);
+	}while(choice!=8);
 }
